8fad5f2371f4295f9aa5debd48d15d01bde83cd4
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-04 20:51:29 -0300

    Restaurante e Diversao component

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { OfertasService } from "../ofertas.service";
-import { Oferta } from './../shared/oferta.model';
+import { Oferta } from "./../shared/oferta.model";
 
 @Component({
   selector: "app-home",
@@ -9,11 +9,17 @@ import { Oferta } from './../shared/oferta.model';
   providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
-  public ofertas: Oferta[]
+  public ofertas: Oferta[];
   constructor(private ofertasService: OfertasService) {}
 
   ngOnInit(): void {
-    this.ofertas = this.ofertasService.getOfertas()
-
+    this.ofertasService
+      .getOfertas()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      })
+      .catch((param: any) => {
+        console.log(param);
+      });
   }
 }
