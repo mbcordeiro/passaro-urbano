2ba12e219c7542bce1851c2d38166b920bee379e
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-05 22:02:29 -0300

    Url API

--- app/src/app/ofertas.service.ts
+++ app/src/app/ofertas.service.ts
@@ -1,6 +1,8 @@
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from "./shared/oferta.model";
+import { URL_API } from './app.api';
+
 
 import 'rxjs/add/operator/toPromise'
 
@@ -9,19 +11,19 @@ export class OfertasService {
   constructor(private http: Http) {}
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:3000/ofertas?destaques=true')
+    return this.http.get(`${URL_API}?destaques=true`)
       .toPromise()
       .then((resposta: any) => resposta.json())
   }
 
   public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta.json())
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta.json()[0]
